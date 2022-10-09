773266b052f590f60bc3bebcaab9078f01c2ccd4
Lakshan Pathum <107621003+lakshan17@users.noreply.github.com>
2022-10-09 13:49:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", " ").split())
+BOT_USERNAME = os.getenv("BOT_USERNAME")
 START_STRING = """
 **🔮 Hello There, You Can Use Me To Create Awesome Logos...**
 ➤ Click /help Or The Button Below To Know How To Use Me
